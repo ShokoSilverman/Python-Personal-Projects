7f1667b67726c7b5b71a2c74e67fc213b3b91988
Simon Silverman <ssilverman@student.neumont.edu>
2022-04-06 13:25:44 -0600

    started mongo updating

--- Imports/Silver_ODBC.py
+++ Imports/Silver_ODBC.py
@@ -16,17 +16,17 @@ class SQL:
     # mydb = 'test'
     def test_con(self):
         print(self.mydb)
-    def query(self, query_str): #pass in a sql query, does not return
+    def query(self, query_str:str): #pass in a sql query, does not return
         self.mycursor.execute(query_str)
         self.mydb.commit()
-    def query_return(self, query_str): #pass in a sql query, returns a value
+    def query_return(self, query_str:str): #pass in a sql query, returns a value
         self.mycursor.execute(query_str)
         myresult = self.mycursor.fetchall()
         return myresult
-    def query_df(self, query_str): #pass in a sql query, returns a pandas dataframe
+    def query_df(self, query_str:str): #pass in a sql query, returns a pandas dataframe
         table = pd.read_sql_query(query_str, self.mydb)
         return table
-    def insert_one(self, table, values): #hand in the table name and a tuple of values 
+    def insert_one(self, table:str, values:tuple): #hand in the table name and a tuple of values 
         sql_values = "("
         for value in values:
             sql_values+=str(value)+','
@@ -34,14 +34,14 @@ class SQL:
         query_str = f"INSERT INTO {table} VALUES {str(values)}"
         self.mycursor.execute(query_str)
         self.mydb.commit()
-    def read(self, table, data_wanted='*', primary_key=None): #pass in the table to grab data from and can optionally pass in a tuple with the fields you want back, will default to all fields
-        values_wanted = str(data_wanted).removeprefix('(').removesuffix(')').replace('\'', '')
+    def read(self, table:str, data_wanted:list='*', primary_key:str=None): #pass in the table to grab data from and can optionally pass in a list with the fields you want back, will default to all fields, can optionally pass in a primary key to index on
+        values_wanted = str(data_wanted).removeprefix('[').removesuffix(']').replace('\'', '').replace('\"','')
         if primary_key is None:
             table = pd.read_sql_query(f"SELECT {values_wanted} FROM {table}", self.mydb)
         else:
             table = pd.read_sql_query(f"SELECT {values_wanted} FROM {table}", self.mydb, index_col=primary_key)
         print(table)
-    def update(self, table, upd_on, new_vals): #upd_on = {field, where_val} #dict of new vals {field:val, field:val}
+    def update(self, table:str, upd_on:dict, new_vals:dict): #upd_on = {field, where_val} #dict of new vals {field:val, field:val}
         where_field = list(upd_on.keys())[0]
         val_field = upd_on[where_field]
         query_str = f"UPDATE {table} SET"
@@ -51,7 +51,7 @@ class SQL:
         query_str += f" WHERE {where_field} = {val_field}"
         self.mycursor.execute(query_str)
         self.mydb.commit()
-    def delete(self, table, del_on): #del_on {field, delete val}
+    def delete(self, table:str, del_on:dict): #del_on {field, delete val}
         where_field = list(del_on.keys())[0]
         val_field = del_on[where_field]
         query_str = f"DELETE FROM {table} WHERE {where_field} = {val_field}"
@@ -59,7 +59,7 @@ class SQL:
         self.mydb.commit()
 
 class my_SQL(SQL):
-    def __init__(self, username, password, host_ip, schema):  #pass in username, password, ip, and schema for the database
+    def __init__(self, username:str, password:str, host_ip:str, schema:str):  #pass in username, password, ip, and schema for the database
         try:
             import mysql.connector
         except:
@@ -67,9 +67,9 @@ class my_SQL(SQL):
             if len(import_error_msg) > 0:
                 print(import_error_msg.strip())
                 quit()
-            self.mydb = mysql.connector.connect(user=username, password=password, host=host_ip, database=schema)
+        self.mydb = mysql.connector.connect(user=username, password=password, host=host_ip, database=schema)
         self.mycursor = self.mydb.cursor()
-    def insert_many(self, table, values, num_fields): #pass in table name, the list of tuples of values [(values)] and the number of fields in the table (excluding auto incrementing fields)
+    def insert_many(self, table:str, values:list, num_fields:int): #pass in table name, the list of tuples of values [(values)] and the number of fields in the table (excluding auto incrementing fields)
         value_str = "("
         for i in range(num_fields):
             value_str+="%s,"
@@ -88,13 +88,13 @@ class SQL_server(SQL):
         if len(import_error_msg) > 0:
             print(import_error_msg.strip())
             quit()
-    def __init__(self, server_name, database): #pass in the servername (SELECT @@SERVERNAME) and the database name
+    def __init__(self, server_name:str, database:str): #pass in the servername (SELECT @@SERVERNAME) and the database name
         self.mydb = pyodbc.connect('Driver={SQL Server};'
                     f'Server={server_name};'
                     f'Database={database};'
                     'Trusted_Connection=yes;')
         self.mycursor = self.mydb.cursor()
-    def insert_many(self, table, values, num_fields): #pass in table name, the list of tuples of values [(values)] and the number of fields in the table (excluding auto incrementing fields)
+    def insert_many(self, table:str, values:list, num_fields:int): #pass in table name, the list of tuples of values [(values)] and the number of fields in the table (excluding auto incrementing fields)
         value_str = "("
         for i in range(num_fields):
             value_str+="?,"
@@ -104,6 +104,36 @@ class SQL_server(SQL):
         self.mycursor.executemany(query_str, values)
         self.mydb.commit()
         
-#mongo
+
+class mongoDB:
+    def __init__(self, host:str, port:int, database:str):
+        try:
+            from pymongo import MongoClient
+        except:
+            import_error_msg += 'missing mysql pip install! \n'
+            if len(import_error_msg) > 0:
+                print(import_error_msg.strip())
+                quit()
+        self.myclient = MongoClient(host, port)
+        self.mydb = self.myclient[database]
+    def insert_one(self, collection:str, values:dict): #{key:val, key:val}
+        mycol = self.mydb[collection]
+        mycol.insert_one(values)
+    def insert_many(self, collection:str, values:list): #pass a list of dicts [{entry},{entry}]
+        mycol = self.mydb[collection]
+        mycol.insert_many(values)
+    def read(self, collection:str, where:dict={}): #pass in search arguments {key:val_wanted, key:val_wanted}
+        mycol = self.mydb[collection]
+        return mycol.find(where)
+    def update(self, collection:str, where:dict, new_values:dict, upsert:bool=True,):
+        mycol = self.mydb[collection]
+        set_vals = {"$set":new_values}
+        mycol.update_many(where, set_vals, upsert)
+        # mycol.update_many({'name':'Grayson'}, {"$set":{'name':'Alex'}}, upsert)
+    def update_one(self, collection:str, where:dict, new_values:dict, upsert:bool=True):
+        pass
+
+
+
 #neo4j
 #redis
--- Silver_ODBC_Testing.py
+++ Silver_ODBC_Testing.py
@@ -1,11 +1,15 @@
 # import Imports.Silver_ODBC as Sil_ODBC
 from Imports.Silver_ODBC import SQL_server as Silver_ODBC_SQL_server
-from Imports.Silver_ODBC import my_SQL
+from Imports.Silver_ODBC import my_SQL as Silver_my_SQL
+from Imports.Silver_ODBC import mongoDB as Silver_mongoDB
 
-SQL_server = Silver_ODBC_SQL_server(server_name='LAPTOP-L46BVJII', database='sql_test')
-my_SQL = my_SQL('root', 'test', '127.0.0.1', 'python_db')
+# SQL_server = Silver_ODBC_SQL_server(server_name='LAPTOP-L46BVJII', database='sql_test')
+# my_SQL = Silver_my_SQL('root', 'test', '127.0.0.1', 'python_db')
+mongo = Silver_mongoDB('localhost',27017, 'testMongoSilver')
 def main():
+
     #------------SQL SERVER-----------------
+
     # SQL_server.test_con()
     # print(SQL_server.query("insert into simple_people Values(9,'Liam', 'Douglas',2022)"))
     # print(SQL_server.query_df("select * from simple_people"))
@@ -13,7 +17,7 @@ def main():
     # bulk_insert_list = [(4, 'Tristyn', 'Kahrs', 2022), (5, 'Robert', 'Brunney', 2022)]
     # SQL_server.insert_many('simple_people', bulk_insert_list, 4)
     # SQL_server.read('simple_people')
-    # SQL_server.read('simple_people', ('Id', 'LastName'), 'Id')
+    # SQL_server.read('simple_people', ['Id', 'LastName'], 'Id')
     # SQL_server.update('simple_people', {'Id':1}, {'FirstName':"'Grayson'", 'LastName':"'Sudweeks'"})
     # SQL_server.delete('simple_people', {'FirstName':"'David'"})
 
@@ -26,9 +30,20 @@ def main():
     # bulk_insert_list = [(4, 'Tristyn', 'Kahrs', 2022), (5, 'Robert', 'Brunney', 2022)]
     # my_SQL.insert_many('simple_people', bulk_insert_list, 4)
     # my_SQL.read('simple_people')
-    # my_SQL.read('simple_people', ('Id', 'LastName'), 'Id')
+    # my_SQL.read('simple_people', ["Id", "LastName"], 'Id')
     # my_SQL.update('simple_people', {'Id':1}, {'FirstName':"'Grayson'", 'LastName':"'Sudweeks'"})
     # my_SQL.delete('simple_people', {'FirstName':"'David'"})
+
+    #-----------------Mongo-------------------------------
+    
+    # mongo.insert_one('testCol', {'name':'Simon', 'age':19, 'hireyear':2022})
+    # bulk_insert_list = [{'name':'Tobie', 'age':20}, {'name':'Grayson', 'age':19}]
+    # mongo.insert_many('testCol', bulk_insert_list)
+    # for item in mongo.read('testCol'):
+    #     print(item)
+    # for item in mongo.read('testCol', {'age':19, 'name':'Simon'}):
+    #     print(item)
+    mongo.update('testCol', {'name':'Grayson'}, {'name':'Alex'})
     pass
 
 
